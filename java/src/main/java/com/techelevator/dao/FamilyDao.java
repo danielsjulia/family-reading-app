751566954158760d7198589e20855b86b22cb8dc
Cymanthia Smith <smithcyk@gmail.com>
2022-05-30 17:40:23 -0400

    Created database and member/family models and dao

---
+++ java/src/main/java/com/techelevator/dao/FamilyDao.java
@@ -0,0 +1,10 @@
+package com.techelevator.dao;
+
+public interface FamilyDao {
+
+    String getFamilyNameByFamilyId(Long familyId);
+
+//    List<Members> getFamilyMembersByFamilyId(Long familyId);
+    // sql statement
+    //
+}
---
+++ java/src/main/java/com/techelevator/model/Member.java
@@ -0,0 +1,9 @@
+package com.techelevator.model;
+
+public class Member {
+    // adding new fam member - this class necessary? passing too much data? do we only need to pass isParent
+    private Long userId;
+    private String username;
+    private boolean isParent;
+    private Long familyId;
+}
