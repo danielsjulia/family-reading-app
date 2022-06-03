71cfd87fc8a6b26f367bf154fde241611f031e9e
Samikshya Aryal <ikshyaa@gmail.com>
2022-06-03 02:49:32 -0700

    member-card and follow up profile skeleton

--- vue/src/components/MemberCard.vue
+++ vue/src/components/MemberCard.vue
@@ -1,13 +1,12 @@
 <template>
   <div 
     class= "member-card" 
-    v-bind:class="{ parent: member.parent}"
+    v-bind:class="{parent : thisMember.parent,  child: !thisMember.parent} "
   >
     <img class="profile-img" src="../../images/girl-avatar-over10.png" />
-    
-      <h1 id="username">{{member.username}}</h1>
-      <h2 id= "parent">{{member.parent ? "Parent" : "Child" }}</h2>
-      <h3 id="userId">UserId: {{member.userId}}</h3>
+      <h1 id="username">{{thisMember.username}}</h1>
+      <!-- <h2 id= "parent">{{member.parent ? "Parent" : "Child" }}</h2> -->
+      <h3 id="userId">UserId: {{thisMember.userId}}</h3>
       <!-- <h3 id= "familyId">family_id: {{member.familyId }}</h3> -->
   </div>
 </template>
@@ -17,6 +16,11 @@ export default {
     name: 'member-card',
     props: {
       member: Object
+    },
+    data() {
+      return {
+        thisMember : this.member
+      }
     }
     
 }
@@ -28,10 +32,25 @@ export default {
     border: 1px solid black;
     border-radius: 100px;
 }
-.member-card {
-  text-align: center;
-}
+/* .member-card 
+  /* display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-areas: 
+  "parent parent"
+  "child child"
+  "child child"
+  ;
+  grid-area: member-card; */
+
 .parent {
-  background : blue !important;
+  /* display: grid; */
+  grid-area: parent;
+  background : #0089BA !important;
+  /* grid-area: parent; */
 }
+.child {
+  /* grid-area: child; */
+  background:#F3C5FF !important;
+}
+
 </style>
