edd41a386f0a8bbc06b0b641482de7d756b364ef
Magalakshmi Senthilkumar <magalakshmi.s@gmail.com>
2022-06-05 20:24:51 -0500

    all books css added

--- vue/src/components/AssignBook.vue
+++ vue/src/components/AssignBook.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
     <!--v-if="this.$store.state.isParent === true"-->
-    <form @submit.prevent="addBook()">
-      <p>Enter username to assign book to:</p>
-      <input type="text" v-model="userBook.username" value="username" />
-      <input type="submit" value="submit" />
+    <form  class="assign-book" @submit.prevent="addBook()">
+      <label for="uname">Assign Book:</label>
+      <input class="text" type="text" v-model="userBook.username" value="username" /><br>
+      <input class="submit" type="submit" value="submit" />
     </form>
   </div>
 </template>
@@ -41,4 +41,23 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@300');
+
+.assign-book{
+  font-family: 'Barlow Condensed', sans-serif;
+  margin:0;
+  font-weight: bold; 
+}
+.text{
+    width:100px;
+    height:10px;
+    border-radius: 10px;
+    border:2px solid pink;
+}
+.submit{
+    border:2px solid pink;
+    border-radius: 10px;
+
+}
+
 </style>
